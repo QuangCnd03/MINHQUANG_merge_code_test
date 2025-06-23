a174cf582c2d4d81e40e2270dfd7b6d5f0bf5d80
unknown <phppythonviettridao@gmail.com>
2025-06-23 15:20:19 +0700

    Initial commit

---
+++ client.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo merge code</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Demo</h1>
+        <p>Duoc tao boi member 1</p>
+    </div>
+</body>
+</html>
