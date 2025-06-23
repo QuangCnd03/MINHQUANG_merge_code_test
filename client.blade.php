5c7888376aca7eec2359071fe359679f8a260095
unknown <phppythonviettridao@gmail.com>
2025-06-23 15:39:10 +0700

    Update code 3 39

--- client.blade.php
+++ client.blade.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo merge code</title>
 </head>
+
 <body>
     <div class="container">
         <h1>Demo</h1>
         <p>Duoc tao boi member 1</p>
     </div>
+    <div class="member1">
+        <h2>Noi dung bai hoc</h2>
+        <p>Officia beatae dolores integer recusandae, quisquam iusto mi recusandae? Risus, 
+            risus praesent lacus, quibusdam, dolores netus, vitae blandit, tempore magnam illum 
+            sapiente arcu officia, qui itaque n</p>
+    </div>
 </body>
+
 </html>
